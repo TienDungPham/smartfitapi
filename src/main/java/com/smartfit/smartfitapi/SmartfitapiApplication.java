5e1bb5dd2f0ffffab321959d798f6d046e96ffb6
Dung (FPT Aptech) <dungptth1805032@fpt.edu.vn>
2020-09-29 21:55:01 +0700

    Init project

---
+++ src/main/java/com/smartfit/smartfitapi/SmartfitapiApplication.java
@@ -0,0 +1,13 @@
+package com.smartfit.smartfitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfitapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartfitapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartfit/smartfitapi/SmartfitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfit.smartfitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfitapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
